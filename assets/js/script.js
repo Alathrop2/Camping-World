11a9d9f42a9e870bd94902c902c8939a17511b1d
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-08-24 20:57:44 -0600

    added state codes, and arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,130 @@
-function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var apiKey = '6pJRVZpzh01tEktlNNLSmI1hVw5wXNTOuoca58uW';
-  var requestUrl =
-    'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=' + apiKey;
+var firewood = document.getElementById('firewood');
+var showers = document.getElementById('showers');
+var laundry = document.getElementById('laundry');
+var Dropdown = document.getElementById('state');
+var apiKey = '6pJRVZpzh01tEktlNNLSmI1hVw5wXNTOuoca58uW';
+var stateCode = '';
+var state = '&api_key=';
+var stateNameArray = [
+  'Alabama',
+  'Alaska',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'Florida',
+  'Georgia',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Pennsylvania',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+];
+var stateCodeArray = [
+  'AL',
+  'AK',
+  'AZ',
+  'AR',
+  'CA',
+  'CO',
+  'CT',
+  'DE',
+  'FL',
+  'GA',
+  'HI',
+  'ID',
+  'IL',
+  'IN',
+  'IA',
+  'KS',
+  'KY',
+  'LA',
+  'ME',
+  'MD',
+  'MA',
+  'MI',
+  'MN',
+  'MS',
+  'MO',
+  'MT',
+  'NE',
+  'NV',
+  'NH',
+  'NJ',
+  'NM',
+  'NY',
+  'NC',
+  'ND',
+  'OH',
+  'OK',
+  'OR',
+  'PA',
+  'RI',
+  'SC',
+  'SD',
+  'TN',
+  'TX',
+  'UT',
+  'VT',
+  'VA',
+  'WA',
+  'WV',
+  'WI',
+  'WY',
+];
+var requestUrl =
+  'https://developer.nps.gov/api/v1/campgrounds/?statecode=' +
+  stateCode +
+  state +
+  apiKey;
 
+function getApi() {
+  console.log(Dropdown.value);
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // for (var i = 0; i < data.length; i++) {
-      //   var link = document.createElement('a');
-      //   var listItem = document.createElement('li');
-      //   listItem.textContent = data[i].html_url;
-      //   link.href = data[i].html_url;
-      //   repoList.appendChild(link);
-      //   link.appendChild(listItem);
-      // }
       console.log(data);
     });
 }
+// firewood option
+
+firewood.addEventListener('click', getApi);
