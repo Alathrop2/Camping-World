4b96152743600c209e3ac0fdd846bf86ca2c9b80
Manamana <allentrobson@gmail.com>
2022-08-29 18:43:44 -0600

    fixed async

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,16 +1,114 @@
 
-var city= "salt+lake+city";
+var apiCity = "seattle";
 var apiKey = '4d63ba9d93efddcbcaf8047f7d2ec8b0';
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var requestUrl;
+var latitude;
+var longitude;
 
-  fetch(requestUrl)
+let currentCityWeather = {};
+let weatherObject = {};
+
+
+async function getWeather(city) {
+
+  apiCity = city;
+
+  requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
+
+  currentCityWeather = await fetch(requestUrl)
     .then(function (response) {
+
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      
 
-      console.log(data.main.feels_like);
+      return data;
+
+      
+
     });
 
-    // https://api.openweathermap.org/data/2.5/onecall?lat=40.7608&lon=111.8910&appid=4d63ba9d93efddcbcaf8047f7d2ec8b0 
+
+
+}
+
+async function renderWeather(city){
+  apiCity = city;
+  await getWeather(apiCity);
+
+  console.log(currentCityWeather);
+
+  weatherObject = {
+    city: apiCity,
+    currentTemp: currentCityWeather.main.temp,
+    minTemp: currentCityWeather.main.temp_min,
+    maxTemp: currentCityWeather.main.temp_max,
+    weatherDescription: currentCityWeather.weather[0].description
+  }
+
+  return weatherObject;
+
+}
+
+console.log(renderWeather("los+angeles"));
+
+
+
+
+
+async function getWeatherLatLon(lat,lon) {
+
+  
+
+  requestUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+
+  
+
+  currentCityWeather = await fetch(requestUrl)
+    .then(function (response) {
+
+      return response.json();
+    })
+    .then(function (data) {
+      
+
+      return data;
+
+      
+
+    });
+
+
+
+}
+
+
+
+
+async function renderWeatherLatLon(lat,lon){
+  latitude = lat;
+  longitude = lon;
+  await getWeatherLatLon(latitude,longitude);
+
+  console.log(currentCityWeather);
+
+  weatherObject = {
+    city: currentCityWeather.name,
+    currentTemp: currentCityWeather.main.temp,
+    minTemp: currentCityWeather.main.temp_min,
+    maxTemp: currentCityWeather.main.temp_max,
+    weatherDescription: currentCityWeather.weather[0].description
+  }
+
+  return weatherObject;
+
+}
+
+console.log(renderWeatherLatLon(34.0522,-118.2437));
+
+
+
+
