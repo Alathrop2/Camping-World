df494a58b612fc0e9e8a0509645e1574c9c4bb15
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-08-31 19:29:37 -0600

    fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,13 @@
-var apiCity = 'seattle';
-var WeatherApiKey = '4d63ba9d93efddcbcaf8047f7d2ec8b0';
-var requestUrl;
-var latitude;
-var longitude;
-
-let currentCityWeather = {};
-let weatherObject = {};
-
-// End of weather api variables
-
 var firewood = document.getElementById('firewood');
 var showers = document.getElementById('showers');
-var laundry = document.getElementById('laundry');
-var Dropdown = document.getElementById('state');
+var CellPhone = document.getElementById('CellPhone');
+var Dropdown = document.getElementById('Dropdown');
+var firewoodCheckBox = document.getElementById('firewood');
+var showersCheckBox = document.getElementById('showers');
+var CellPhoneCheckBox = document.getElementById('CellPhone');
 var button = document.getElementById('button');
 var apiKey = '6pJRVZpzh01tEktlNNLSmI1hVw5wXNTOuoca58uW';
+
 var stateCode = '';
 var state = '&api_key=';
 var stateNameArray = [
@@ -128,10 +121,6 @@ var requestUrl =
   state +
   apiKey;
 
-// todo var createDiv = $('<div>').addClass(
-//   ' box column has-background-primary is-size-2 is-2 has-text-centered'
-// );
-
 // Campground API fetch request function
 function getApi() {
   // console.log(Dropdown.value);
@@ -150,73 +139,120 @@ function getApi() {
       return response.json();
     })
     .then(function (data) {
+      // console.log(data);
+      saveChecked();
       displayData(data);
     });
 }
 // firewood option
 var displayData = function (data) {
-  console.log(data);
-  // var firewoodSale = data.amenities.firewoodForSale;
-  // console.log(firewoodSale);
-  // console.log(data.data[i]);
+  var finalCampsite = [];
+  var campBox = $('.camp');
+  if (campBox.children()) {
+    campBox.children().remove();
+  }
+  var createDiv = $('<div>').addClass(
+    ' has-text-centered box is-size-2 has-background-success-dark has-text-warning-dark'
+  );
+
   for (let i = 0; i < data.data.length; i++) {
-    // console.log(data.data[i].amenities.firewoodForSale);
+    var url = data.data[i].url;
+    var website = $('<a>').attr('href', url).text(url);
+    // var text = $('<h1>').text(website);
+    var willAddCampSite = true;
+    var firewoodAvailable =
+      data.data[i].amenities.firewoodForSale.includes('Yes');
+    // var showerAvailable =
+    //   data.data[i].amenities.showers[0] !== 'None' &&
+    //   data.data[i].amenities.showers.length;
+    var cellPhoneAvailable =
+      data.data[i].amenities.cellPhoneReception.includes('Yes');
     // * looking for firewood available for purchase
-    if (data.data[i].amenities.firewoodForSale.includes('Yes')) {
-      // console.log(data.data[i]);
+    if (firewoodCheckBox.checked) {
+      if (!firewoodAvailable) {
+        willAddCampSite = false;
+      }
+      // console.log();
       // * looking for showers
-    } else if (
-      data.data[i].amenities.showers[0] !== 'None' &&
-      data.data[i].amenities.showers.length
-    ) {
+      // } else if (showersCheckBox.checked) {
+      //   if (!showerAvailable) {
+      //     willAddCampSite = false;
+      //   }
       // console.log(data.data[i]);
       // *looking for cellphone reception
-    } else if (data.data[i].amenities.cellPhoneReception.includes('Yes')) {
+    } else if (CellPhoneCheckBox.checked) {
+      if (!cellPhoneAvailable) {
+        willAddCampSite = false;
+      }
       // console.log(data.data[i]);
     }
+    if (willAddCampSite) {
+      finalCampsite.push(data.data[i]);
+      createDiv.append(website);
+      campBox.append(createDiv);
+    }
   }
-
-  // console.log('final campsite', finalCampsite);
+  console.log('final campsite', finalCampsite);
 };
 
-// Weather api functions
+/* Local Storage section */
 
-async function getWeatherLatLon(lat, lon) {
-  requestUrl =
-    'http://api.openweathermap.org/data/2.5/weather?lat=' +
-    lat +
-    '&lon=' +
-    lon +
-    '&appid=' +
-    WeatherApiKey;
+// This code will check if previous states exists in local storage
+// if it does, initialize chosen states with the value of previous states in local storage
+if (localStorage.getItem('previousStates')) {
+  var chosenStates = JSON.parse(localStorage.getItem('previousStates'));
+  console.log('previous states exist');
+} else {
+  // if it does not, create a new array with the states that the user choses
+  var chosenStates = [];
+  console.log('no previous states exist');
+}
+console.log(typeof chosenStates);
 
-  currentCityWeather = await fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      return data;
-    });
+// This will save the checked boxes within the local storage, or update the checkboxes when they have not been checked
+function saveChecked() {
+  localStorage.setItem('savedFirewood', 'false');
+  localStorage.setItem('savedShowers', 'false');
+  localStorage.setItem('savedCellPhone', 'false');
+
+  if (firewoodCheckBox.checked) {
+    localStorage.setItem('savedFirewood', 'true');
+  }
+
+  if (showersCheckBox.checked) {
+    localStorage.setItem('savedShowers', 'true');
+  }
+
+  if (CellPhoneCheckBox.checked) {
+    localStorage.setItem('savedCellPhone', 'true');
+  }
+
+  // When the user clicks on a state, the state that they submit with the button will appear in their local storage
+  if (Dropdown) {
+    chosenStates.push(Dropdown.value);
+    localStorage.setItem('previousStates', JSON.stringify(chosenStates));
+  }
 }
 
-async function renderWeatherLatLon(lat, lon) {
-  latitude = lat;
-  longitude = lon;
-  await getWeatherLatLon(latitude, longitude);
+// When the user loads into the page, the saved checked boxes, from the previous session, will appear checkedmarked as the last thing they saved
+function loadChecked() {
+  var firewoodchecked = localStorage.getItem('savedFirewood');
+  var showersChecked = localStorage.getItem('savedShowers');
+  var CellPhoneChecked = localStorage.getItem('savedCellPhone');
 
-  // console.log(currentCityWeather);
+  if (firewoodchecked == 'true') {
+    document.getElementById('firewood').checked = true;
+  }
 
-  weatherObject = {
-    city: currentCityWeather.name,
-    currentTemp: currentCityWeather.main.temp,
-    minTemp: currentCityWeather.main.temp_min,
-    maxTemp: currentCityWeather.main.temp_max,
-    weatherDescription: currentCityWeather.weather[0].description,
-  };
+  if (showersChecked == 'true') {
+    document.getElementById('showers').checked = true;
+  }
 
-  return weatherObject;
+  if (CellPhoneChecked == 'true') {
+    document.getElementById('CellPhone').checked = true;
+  }
 }
 
-// End of weather api functions
+loadChecked();
 
 button.addEventListener('click', getApi);
